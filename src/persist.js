ef8780e3d3fde1591c226d60b51d359a21744779
Pim Brouwers <mpbrouwers@gmail.com>
2017-01-24 14:29:51 -0500

    Update persist.js
    Cookies removed / AMD Module Name removed / Handles renamed

--- src/persist.js
+++ src/persist.js
@@ -4,7 +4,7 @@
 */
 (function (root, factory) {
     if (typeof define === "function" && define.amd) {
-        define('persist', factory);
+        define(factory);
     } else if (typeof module === "object" && module.exports) {
         module.exports = (root.PersistJS = factory());
     } else {
@@ -14,83 +14,10 @@
     var PersistJS = {};
 
     /**
-        * Cookies
-        */
-    PersistJS.Cookies = {
-        GetCookie: function (cookieName) {
-            /// <summary>Gets the value of the given cookieName from the current cookies collection.</summary>
-            /// <param name="cookieName" type="String">The name of the cookie to return.</param>
-
-            // first we'll split this cookie up into name/value pairs
-            // note: document.cookie only returns name=value, not the other components
-            var a_all_cookies = document.cookie.split(';');
-            var a_temp_cookie = '';
-            var cookie_name = '';
-            var cookie_value = '';
-            var b_cookie_found = false; // set boolean t/f default f
-
-            for (i = 0; i < a_all_cookies.length; i++) {
-                // now we'll split apart each name=value pair
-                a_temp_cookie = a_all_cookies[i].split('=');
-
-
-                // and trim left/right whitespace while we're at it
-                cookie_name = a_temp_cookie[0].replace(/^\s+|\s+$/g, '');
-
-                // if the extracted name matches passed cookieName
-                if (cookie_name == cookieName) {
-                    b_cookie_found = true;
-                    // we need to handle case where cookie has no value but exists (no = sign, that is):
-                    if (a_temp_cookie.length > 1) {
-                        cookie_value = unescape(a_temp_cookie[1].replace(/^\s+|\s+$/g, ''));
-                    }
-                    // note that in cases where cookie is initialized but no value, null is returned
-                    return cookie_value;
-                }
-                a_temp_cookie = null;
-                cookie_name = '';
-            }
-            if (!b_cookie_found) {
-                return null;
-            }
-        },
-        SetCookie: function (name, value, expires, path, domain, secure) {
-            /// <summary>Sets a cookie based on the name and value provided.</summary>
-            /// <param name="name" type="String">The name of the cookie to set the value of.</param>
-            /// <param name="value" type="String">The value of the cookie to set.</param>
-            /// <param name="expires" type="Date">(Optional) The date that the cookie should expire.</param>
-            /// <param name="path" type="String">(Optional) The path of the cookie.</param>
-            /// <param name="domain" type="String">(Optional) The domain of the cookie.</param>
-            /// <param name="secure" type="Boolean">(Optional) Whether the cookie is secure or not.</param>
-
-            var expires_date = expires;
-
-            document.cookie = name + "=" + escape(value) +
-            ((expires) ? ";expires=" + expires_date.toGMTString() : "") +
-            ((path) ? ";path=" + path : "") +
-            ((domain) ? ";domain=" + domain : "") +
-            ((secure) ? ";secure" : "");
-        },
-        DeleteCookie: function (name, path, domain, secure) {
-            /// <summary>Delete a cookie based on the name provided.</summary>
-            /// <param name="name" type="String">The name of the cookie to set the value of.</param>
-            /// <param name="path" type="String">(Optional) The path of the cookie.</param>
-            /// <param name="domain" type="String">(Optional) The domain of the cookie.</param>
-            /// <param name="secure" type="Boolean">(Optional) Whether the cookie is secure or not.</param>
-
-            if (Inkwell.GetCookie(name)) {
-                document.cookie = name + "=" + (path ? ";path=" + path : "") +
-                    (domain ? ";domain=" + domain : "") + (secure ? ";secure" : "") +
-                    ";expires=Thu, 01-Jan-1970 00:00:01 GMT";
-            }
-        }
-    };
-
-    /**
     * Local Storage Hooks
     */
     PersistJS.Local = {
-        Save: function (key, data) {
+        Set: function (key, data) {
             if (key && data) {
                 var data = JSON.stringify(data);
 
@@ -104,7 +31,7 @@
                 return JSON.parse(window.localStorage[key]);
             }
         },
-        Delete: function (key, callback) {
+        Remove: function (key, callback) {
             if (window.localStorage[key]) {
                 window.localStorage.removeItem(key);
                 if (callback) callback();
@@ -119,7 +46,7 @@
     * Session Storage Hooks
     */
     PersistJS.Session = {
-        Save: function (key, data) {
+        Set: function (key, data) {
             if (key && data) {
                 var data = JSON.stringify(data);
 
@@ -133,7 +60,7 @@
                 return JSON.parse(window.sessionStorage[key]);
             }
         },
-        Delete: function (key, callback) {
+        Remove: function (key, callback) {
             if (window.sessionStorage[key]) {
                 window.sessionStorage.removeItem(key);
                 if (callback) callback();
